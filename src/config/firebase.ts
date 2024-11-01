2b152ad7c9b001a46f332ca5ed9dbbed0c00a27a
Yash Sheth <yashsheth66@gmail.com>
2024-11-01 09:24:34 +0530

    remove firebase api keys

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCNI3cKQZroprhijj1WY4vaFuZfbu0cjCg",
-  authDomain: "tinxy-website.firebaseapp.com",
-  projectId: "tinxy-website",
-  storageBucket: "tinxy-website.appspot.com",
-  messagingSenderId: "96251686093",
-  appId: "1:96251686093:web:f6bfd9119ede1aaad1cd84",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 }
 
 // Initialize Firebase
